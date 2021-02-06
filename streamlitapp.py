9cad89a4e78cff0190f1964fcddfe258267d5cd6
vatsals <vatsals@quinnox.com>
2021-02-07 01:42:49 +0530

    added image comparision

---
+++ streamlitapp.py
@@ -0,0 +1,34 @@
+from streamlitutils import *
+import streamlit as st
+import io
+from PIL import Image
+
+st.set_page_config(
+    page_title = 'CLIP',
+    page_icon = '🎑'
+)
+
+st.header("CLIP - Semantic Image Search")
+imageText = st.text_input("Search Image")
+
+
+if imageText:
+    with st.spinner(text = 'Getting Images from upsplash and sorting with clip ...'):
+        
+        imgSimScore, upSplashImages = getSortedQuery(imageText)
+
+        images = [linkToImage(img) for img, score in imgSimScore]
+        simScore = [f'Sim Score: {score:.2f}' for img, score in imgSimScore]
+
+        upSplashImages = [linkToImage(img) for img in upSplashImages]
+        upSplashIx = [i for i in range(len(upSplashImages))]
+
+        col1, col2 = st.beta_columns(2)
+
+        col1.header("Semantic Search")
+        col1.image(images, width = 100, caption = simScore)
+
+        col2.header("Images from Upsplash")
+        col2.image(upSplashImages, width = 100, caption = upSplashIx)
+
+        
---
+++ streamlitutils.py
@@ -0,0 +1,90 @@
+import torch
+import clip
+from PIL import Image
+import os
+import re
+from tqdm import tqdm, trange
+import random
+import requests
+import numpy as np
+from CONSTS import *
+import streamlit as st
+
+global model, preprocess, device
+
+device = 'cpu'
+
+model, preprocess = clip.load("ViT-B/32", device = 'cpu')
+
+
+
+def getImagesFromUnsplash(total: int, query_text: str):
+    '''
+    Images from query text
+    '''
+    num_page = 1
+    imgs_total = total
+    query = query_text
+    url = f"https://api.unsplash.com/search/photos?query={query_text}&page={num_page}&per_page={imgs_total}"
+
+
+    headers = {
+        "Authorization": f"Bearer Client-ID {UPSPLASH_API}",
+
+    }
+
+    req = requests.get(url, headers = headers)
+
+    resp = req.json()
+
+    regUrls = [r['urls']['regular'] for r in resp['results']]
+
+    return regUrls
+
+
+
+def linkToImage(link):
+    '''
+    Image URL to PIL.Image
+    '''
+    content = requests.get(link, stream = True)
+    content = content.raw
+    img = Image.open(content)
+    return img
+
+# @st.cache
+def getImageTextSimScore(link, text):
+    image = preprocess(linkToImage(link)).unsqueeze(0).to(device)
+    tokenizedText = clip.tokenize(text).to(device)
+    with torch.no_grad():
+        image_features = model.encode_image(image)
+        text_features = model.encode_text(tokenizedText)
+    
+    simScore = torch.matmul(text_features, image_features.T)[0][0]
+
+    return simScore.item()
+
+
+
+
+# @st.cache
+# def loadModelCLIP():
+#     model, preprocess = clip.load("ViT-B/32", device = 'cpu')
+#     return model, preprocess
+
+# @st.cache
+def getSortedQuery(text):
+
+    upSplashImages = getImagesFromUnsplash(40, text)
+    
+    imgSimScore = []
+
+    for ix, img in enumerate(tqdm(upSplashImages)):
+
+        imgSimScore.append((img, getImageTextSimScore(img, text)))
+    
+    imgSimScore = sorted(imgSimScore, key = lambda x: x[1], reverse=True)
+
+    return imgSimScore, upSplashImages
+
+
